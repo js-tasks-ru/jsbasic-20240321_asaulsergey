755d739792df54a7dcc63971907a192a661cd35b
Serega <asaulsergey@gmail.com>
2024-04-08 18:14:23 +0300

    3-module/3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  if (str === '') {
+    return str;
+  }
+  return str
+    .split('-')
+    .map((el, i) => i === 0 ? el : el[0].toUpperCase() + el.slice(1, el.length))
+    .join('');
 }

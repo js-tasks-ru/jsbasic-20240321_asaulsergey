88aa20bcb8aa55b16e414b96837285b49088b731
Serega <asaulsergey@gmail.com>
2024-04-15 23:48:47 +0300

    fix module4-task1 && add module4-task3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,13 +1,11 @@
 function makeFriendsList(friends) {
-  const listFriends = document.createElement('ul');
-  
+  const listFriends = document.createElement("ul");
+
   for (let friend of friends) {
-    let li = document.createElement('li');
+    let li = document.createElement("li");
     li.textContent = `${friend.firstName} + ${friend.lastName}`;
     listFriends.append(li);
   }
-  
-  return listFriends;
-  }
-}
 
+  return listFriends;
+}

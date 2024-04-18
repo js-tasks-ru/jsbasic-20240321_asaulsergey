d529fc94634db98b3e5f4c33ab74162caf92012b
Serega <asaulsergey@gmail.com>
2024-04-18 19:30:37 +0300

    5-module/2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector(".toggle-text-button");
+  const text = document.querySelector("#text");
+  console.log(text);
+
+  function HiddenText() {
+    if (text.hasAttribute("hidden")) {
+      text.removeAttribute("hidden");
+    } else {
+      text.setAttribute("hidden", "true");
+    }
+  }
+
+  button.addEventListener("click", HiddenText);
 }

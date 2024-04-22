1030cf9ae78fba69b8f61d2eaa636042bbbbed80
Serega <asaulsergey@gmail.com>
2024-04-22 23:36:42 +0300

    6module-1task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,48 @@
  *      }
  *
  */
+
 export default class UserTable {
+  elem = null;
+  #list = []
+
   constructor(rows) {
+    this.#list = rows;
+
+    this.elem = this.#render();
+  }
+
+  #template() {
+    return this.#list.map((value) => `
+      <tr>
+        <td>${value.name}</td>
+        <td>${value.age}</td>
+        <td>${value.salary}</td>
+        <td>${value.city}</td>
+        <td>
+          <button>X</button>
+        </td>
+      </tr>
+    `).join('\n');
+  }
+
+  #deleteButtonTr (e) {
+    const target = e.target;
+
+    if (!target.closest('button')) {
+      return;
+    } else {
+      target.closest('tr').remove();
+    }
+
+  }
+
+  #render() {
+    this.elem = document.createElement("table");
+    this.elem.innerHTML = this.#template();
+
+    this.elem.addEventListener('click', this.#deleteButtonTr.bind(this));
+
+    return this.elem;
   }
 }
